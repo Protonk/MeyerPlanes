7d29ead39b4c7a0f9007d370cd19c0c9d6906d4f
Adam Hyland <protonk@gmail.com>
2012-08-05 20:03:42 -0400

    categorize titles.

--- patents.import.R
+++ patents.import.R
@@ -1,8 +1,12 @@
+# include text mining library as a more consistent method of extracting information 
+# from titles
+library(tm)
+
 path.patents.table <- file.path(getwd(), "Data", "Patents-Table 1.csv")
 
 # na.strings modified to include empty date strings
 # Columns 15-19 are empty
-patents.df <- read.csv(path.patents.table, as.is = TRUE, na.strings = c("NA", ""))[, 1:14]
+patents.df <- read.csv(path.patents.table, as.is = TRUE, na.strings = c("NA", ""))[1:1715, 1:14]
 
 # exact same conversion as on reduced dataset
 langs.str <- c('Britain','Germany','France','United States')
@@ -29,3 +33,20 @@ names(patents.df) <- c("Year.protection.applied",
 
 patents.df[, "Date.Applied.For"] <- gsub("[^0-9/]", "", patents.df[, "Date.Applied.For"])
 patents.df[, "Date.Granted"] <- gsub("[^0-9/]", "", patents.df[, "Date.Granted"])
+# Drop location information for authors (~ 400 rows)
+patents.df[, "Authors"] <- gsub("\\\n.*$|\\(.*\\)", "", patents.df[, "Authors"])
+
+### Generate title information
+
+## Simple table of terms. Potentially useful for visualization
+# lowercase and drop hyphens
+term.table <- table(tolower(gsub("-", " ", patents.df[, "English.Title.Summary"])))
+term.table <- Filter(function(x) x > 3, term.table)
+term.table <- term.table[order(term.table, decreasing = TRUE)]
+
+## Categories for titles (need input here)
+
+airships.pat <- "balloon|dirigible|(air|aerial)[- ]ship|luftschiff"
+airplane.pat <- "airplane|aeroplane|flugzeug"
+flying.pat <- "(flying|aerial)[- ]machine||apparatus|flugapparat|flugmaschine"
+component.pat <- "device|propeller|^apparatus"
