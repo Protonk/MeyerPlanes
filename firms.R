bbdb466c95a0ef6c764e2f6d73db9a6d20c6e2f6
Adam Hyland <protonk@gmail.com>
2012-08-10 15:49:21 -0400

    imputing years

--- firms.R
+++ firms.R
@@ -17,8 +17,38 @@ firms.df[, "Nation"] <- gsub("^\\s|\\s$", "", firms.df[, "Nation"])
 firms.df[, "Nation"] <- gsub("^US$|^.USA$", "USA", firms.df[, "Nation"])
 firms.df[, "Nation"] <- gsub(".*[Hh]ungary|^AH$", "Austria-Hungary", firms.df[, "Nation"])
 firms.df[, "Nation"] <- gsub("Germanu", "Germany", firms.df[, "Nation"])
+firms.df[, "Nation"] <- gsub(" \\([^()]*\\)", "", firms.df[, "Nation"])
+
+
 # separators and split listings
 
 firms.df[, "Nation"] <- sub(";", "," firms.df[, "Nation"])
+
+# breakMultiples(data = firms.df, column = "Nation")
+
+
+## Years
+
+# more sensible name for now
+names(firms.df)[3] <- "Year Start"
+
+# Generate column for imputed years as we'll
+# drop a number of qualifiers
+
+firms.df[, "Year Imputed"] <- gsub("\\D", "", firms.df[, "Year Start"])
+
+# average year ranges
+
+ranged <- firms.df[grep("\\d{8}", firms.df[, "Year Imputed"]), "Year Imputed"]
+avg.ranged <- round((as.numeric(substr(ranged, 0, 4)) + as.numeric(substr(ranged, 5, 8)))/2)
+firms.df[grep("\\d{8}", firms.df[, "Year Imputed"]), "Year Imputed"] <- avg.ranged
+
+
+
+
+
+
+
+
 
 
