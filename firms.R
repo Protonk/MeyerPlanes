655c9b9ef59750b9951a1b334a93594a7da0f89b
Adam Hyland <protonk@gmail.com>
2012-08-10 19:55:33 -0400

    stacked bar chart.

--- firms.R
+++ firms.R
@@ -76,7 +76,11 @@ other.countries <- names(table(firms.reduced.df[, "Country"]))[table(firms.reduc
 firms.reduced.df[firms.reduced.df[, "Country"] %in% other.countries, "Country"] <- "Other"
 firms.reduced.df[, "Country"] <- factor(firms.reduced.df[, "Country"])
 
+firms.reduced.df <- firms.reduced.df[complete.cases(firms.reduced.df), ]
+
   
+prelim.firm.bar <- ggplot(data = subset(firms.reduced.df, Year > 1860 & Year < 1920), aes(x = Year, y = `Firm Starts`, fill = Country)) + geom_bar(position = "stack", stat= "identity")
+
 
 
 
