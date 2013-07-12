0609ba7dde65f3651d26a9e0221ad89456f106d7
Adam Hyland <protonk@gmail.com>
2013-07-12 14:20:54 -0400

    rm code not currently used in project.

--- Optional scripts/ngram.regex.R
+++
@@ -1,7 +0,0 @@
-ngram.in <- scan(file = file.path(getwd(), "Data", "ngram", "ngramtable.txt"), what = "list", sep = "\n")
-
-terms.ngram <- mapply(function(x) unlist(strsplit(ngram.in[seq(x, 33, by =  3)], ", +")), x = 1:3, SIMPLIFY = FALSE)
-names(terms.ngram) <- c("English", "French", "German")
-terms.ngram <- lapply(terms.ngram, tolower)
-
-ngram.regex <- lapply(terms.ngram, function(X) paste(X, collapse = "|"))
